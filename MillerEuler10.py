e3829d872a17fb68d8abfe55b5514ba30ca7a257
MickeySauce68 <103530779+MickeySauce68@users.noreply.github.com>
2022-05-16 16:43:23 -0400

    Add files via upload

---
+++ MillerEuler10.py
@@ -0,0 +1,38 @@
+
+Primes = []
+
+
+def prime_alg(x):
+    global y
+    if x >= 1:
+        for a in range(2, x):
+            for b in range(2,10):
+                z = b * a
+                if z == y:
+                    return False
+
+                    
+            
+    else:
+        print("INPUT should be larger than 1.")
+
+        
+def prime_sum():
+    global y
+    y = x
+    for i in range(x):
+        if prime_alg(x) == None and y != 1:
+            Primes.append(y)
+            y -= 1
+        else:
+            y -= 1
+def display_sum():
+    Sum = 0
+    for number in Primes:
+        Sum += number
+        
+    print(Sum)
+#--------------------------------------------------------------------------
+x = int(input("INPUT: "))
+prime_sum()
+display_sum()
