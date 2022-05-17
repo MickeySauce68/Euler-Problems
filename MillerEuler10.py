4cd0f2397e269d4381f3fd3b7b1e1e1953cfef57
MickeySauce68 <103530779+MickeySauce68@users.noreply.github.com>
2022-05-16 20:40:09 -0400

    Update MillerEuler10.py

--- MillerEuler10.py
+++ MillerEuler10.py
@@ -1,27 +1,30 @@
 
-Primes = []
 
+
+Primes = [2]
+combs = 0 
 
 def prime_alg(x):
     global y
     if x >= 1:
-        for a in range(2, x):
-            for b in range(2,x):
+        combs = 0
+        for a in range(Primes[-1], x):
+            for b in range(Primes[-1],x):
                 z = b * a
                 if z == y:
+                    combs += 1
                     return False
-
                     
             
     else:
         print("INPUT should be larger than 1.")
 
         
-def prime_sum():
+def prime_sum_list():
     global y
     y = x
-    for i in range(x):
-        if prime_alg(x) == None and y != 1:
+    for i in range(y):
+        if prime_alg(x) == None and combs < 1 and y != 1:
             Primes.append(y)
             y -= 1
         else:
@@ -30,9 +33,11 @@ def display_sum():
     Sum = 0
     for number in Primes:
         Sum += number
-        
+    print(Primes)
     print(Sum)
+    print(combs)
 #--------------------------------------------------------------------------
 x = int(input("INPUT: "))
-prime_sum()
+prime_sum_list()
 display_sum()
+
